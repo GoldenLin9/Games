0c7a6440203ef19bd5db5cc9bfb26fa92824df17
Golden Lin <67626928+GoldenLin9@users.noreply.github.com>
2022-01-28 03:38:07 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -9,6 +9,9 @@ const filters = document.querySelector("#filters");
 const filterTags = document.querySelector("#filter-tags");
 const sort = document.querySelector("#sort");
 const allFilters = [];
+const searchBtn = document.querySelector("#search button");
+const search = document.querySelector("#search");
+const searchBox = document.querySelector("#searchBox");
 
 for(let letter of title){
     let single = document.createElement("span");
@@ -94,6 +97,18 @@ function displayOrder(operation){ // filter arrays using switch and passing new 
                 }
             }
             break;
+        case "new":
+            gamesCopy.sort((f,s)=>{ //Date bigger dates are > smaller and we want big
+                if(new Date(f.date) > new Date(s.date)){ //newer
+                    return -1;
+                } else{
+                    return 1;
+                }
+            });
+            break;
+        case "alphabetical":
+            gamesCopy.sort((a,b)=> (a.name === b.name) ? 0: (a.name > b.name) ? 1: -1);
+            break;
     }
     displayContent(gamesCopy)
 }
@@ -115,11 +130,11 @@ filters.addEventListener("change", ()=>{
                 }
                 let deletedItem = document.querySelector(`#filter-tags span:nth-child(${spanCount})`);
                 allFilters.pop();
-                displayOrder("filter")
+                displayOrder("filter");
+                displayOrder(sort.value);
                 deletedItem.remove();
             })
-            btn.setAttribute("id", "x")
-            btn.textContent = "X";
+            btn.setAttribute("id", "x");
             filterTags.append(box);
             filterTags.append(btn);
         } else{
@@ -127,5 +142,82 @@ filters.addEventListener("change", ()=>{
         }
         allFilters.push(filters.value);
         displayOrder("filter");
+        displayOrder(sort.value);
     }
 });
+
+sort.addEventListener("change", ()=>{
+    displayOrder(sort.value);
+});
+
+function displayGames(e){
+    e.currentTarget.style.borderRadius = "1em 1em 0 0";
+
+    if(e.currentTarget.value.length >= 1){
+        for(let game of games){
+            if(!(game.name.toLowerCase().indexOf(e.currentTarget.value.toLowerCase()) === -1)){
+                const index = game.name.toLowerCase().indexOf(e.currentTarget.value.toLowerCase());
+                if(!(searchBox.contains(document.getElementById(game.name)))){
+                    let anchor = document.createElement("a");
+                    let showBox = document.createElement("div");
+                    let text = document.createElement("p");
+
+                    anchor.setAttribute("href", game.link);
+                    text.innerHTML = `${game.name.slice(0,index)}<strong>${game.name.slice(index, index + e.currentTarget.value.length + 1)}</strong>${game.name.slice(e.currentTarget.value.length + 1 + index, game.name.length)}`;
+                    showBox.setAttribute("id", game.name)
+                    showBox.setAttribute("class", "gameSearches")
+                    
+                    showBox.append(text);
+                    anchor.append(showBox);
+                    searchBox.append(anchor);
+                } else if(searchBox.contains(document.getElementById(game.name))){
+                    document.getElementById(game.name).innerHTML = `${game.name.slice(0,index)}<strong>${game.name.slice(index, index + e.currentTarget.value.length)}</strong>${game.name.slice(e.currentTarget.value.length + index, game.name.length)}`;
+                }
+            
+            } else if(game.name.toLowerCase().indexOf(e.currentTarget.value.toLowerCase()) === -1 && searchBox.contains(document.getElementById(game.name))){
+                document.getElementById(game.name).remove();
+            }
+        }
+        
+        let notFound = document.querySelector("#notFound");
+
+        if(!(searchBox.querySelector(".gameSearches"))){ //if no games
+            if(!(notFound)){ // if not appended the not found sign
+                let notFound = document.createElement("div");
+                notFound.setAttribute("id", "notFound");
+                notFound.innerHTML = `<strong>Sorry, no match found</Strong>`;
+                searchBox.append(notFound);
+            }
+        } else{ //if there are games
+            if(notFound){ // then remove the not found sign
+                notFound.remove();
+            }
+        }
+    } else{
+        for(let div of document.querySelectorAll("#searchBox div")){
+            div.remove();
+        }
+    }
+}
+
+//clicked = add text box & "X" click "X" = add search
+searchBtn.addEventListener("click", ()=>{
+
+    if(!(search.querySelector("#search input"))){ //if button is search else do the x thing
+        searchBtn.style.backgroundImage = `url(../img/x.png)`;
+
+        let input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("placeholder", "Search Games");
+        searchBox.append(input);
+
+        input.focus();
+        input.addEventListener("input", displayGames);
+
+    } else if(search.querySelector("#search input")){ //text box found
+        while(searchBox.firstChild){
+            searchBox.firstChild.remove();
+        }
+        searchBtn.style.backgroundImage = `url("../img/search.png")`;
+    }
+})
