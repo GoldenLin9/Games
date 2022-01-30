bb5946137a1edcd49bf19fdf67339e75340baa79
GoldenLin9 <goldenlin0909@gmail.com>
2022-01-30 05:11:45 -0500

    updated

--- js/Connect4.js
+++ js/Connect4.js
@@ -134,3 +134,45 @@ button.addEventListener("click", ()=>{
     document.querySelector("#player1 h1").textContent = " Player 1";
     document.querySelector("#player2 h1").textContent = "Player 2";
 })
+
+function color(e){
+
+    num = Number(e.currentTarget.getAttribute("id").split("star")[1]);
+
+    if(e.currentTarget.getAttribute("src").includes("black")){
+        for(let index = 1; index <= num; index++){
+            document.querySelector(`#star${index}`).setAttribute("src", "img/gold-star.png")
+        }
+    } else{
+        for(let index = (num+1); index <= stars; index++){
+            document.querySelector(`#star${index}`).setAttribute("src", "img/black-star.png")
+        }
+    }
+}
+
+function color(e){
+
+    num = Number(e.currentTarget.getAttribute("id").split("star")[1]);
+
+    if(e.currentTarget.getAttribute("src").includes("black")){
+        for(let index = 1; index <= num; index++){
+            document.querySelector(`#star${index}`).setAttribute("src", "img/gold-star.png");
+        }
+    } else{
+        for(let index = (num+1); index <= stars; index++){
+            document.querySelector(`#star${index}`).setAttribute("src", "img/black-star.png");
+        }
+    }
+}
+
+rating = document.querySelector("header ul li:nth-child(2)");
+stars = 5;
+for(let star = 1; star <= stars; star++){
+    let img = document.createElement("img");
+    img.setAttribute("src", "img/black-star.png");
+    img.setAttribute("id", `star${star}`);
+    img.setAttribute("class", "star");
+    rating.append(img);
+
+    img.addEventListener("click", color);
+}
--- js/Minesweeper.js
+++ js/Minesweeper.js
@@ -511,3 +511,32 @@ btn.addEventListener("click", ()=>{
         btn.style.backgroundColor = "white";
     }
 });
+
+
+
+function color(e){
+
+    num = Number(e.currentTarget.getAttribute("id").split("star")[1]);
+
+    if(e.currentTarget.getAttribute("src").includes("black")){
+        for(let index = 1; index <= num; index++){
+            document.querySelector(`#star${index}`).setAttribute("src", "img/gold-star.png");
+        }
+    } else{
+        for(let index = (num+1); index <= stars; index++){
+            document.querySelector(`#star${index}`).setAttribute("src", "img/black-star.png");
+        }
+    }
+}
+
+rating = document.querySelector("header ul li:nth-child(2)");
+stars = 5;
+for(let star = 1; star <= stars; star++){
+    let img = document.createElement("img");
+    img.setAttribute("src", "img/black-star.png");
+    img.setAttribute("id", `star${star}`);
+    img.setAttribute("class", "star");
+    rating.append(img);
+
+    img.addEventListener("click", color);
+}
